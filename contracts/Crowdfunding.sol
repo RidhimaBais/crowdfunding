1ab3e9d84a4b9f40172cb4b304ddbef720480f72
RidhimaBais <ridhima8686@gmail.com>
2025-10-30 18:13:40 -1100

    Update Crowdfunding.sol

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -6,9 +6,11 @@ contract MyTokenProject {
     string public symbol = "EDC";
     uint256 public totalSupply;
     address public owner;
+
     mapping(address => uint256) public balanceOf;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     constructor(uint256 _initialSupply) {
         owner = msg.sender;
@@ -16,6 +18,7 @@ contract MyTokenProject {
         balanceOf[owner] = _initialSupply;
     }
 
+    //  Transfer tokens between addresses
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(balanceOf[msg.sender] >= _value, "Insufficient Balance");
         balanceOf[msg.sender] -= _value;
@@ -24,16 +27,30 @@ contract MyTokenProject {
         return true;
     }
 
+    //  Mint new tokens (Owner only)
     function mint(uint256 _amount) public {
         require(msg.sender == owner, "Only owner can mint");
         totalSupply += _amount;
         balanceOf[owner] += _amount;
     }
 
+    //  Burn tokens to reduce supply
     function burn(uint256 _amount) public {
         require(balanceOf[msg.sender] >= _amount, "Insufficient balance to burn");
         balanceOf[msg.sender] -= _amount;
         totalSupply -= _amount;
     }
-}
 
+    //  Check balance of any account
+    function checkBalance(address _account) public view returns (uint256) {
+        return balanceOf[_account];
+    }
+
+    // Transfer contract ownership to a new address
+    function transferOwnership(address _newOwner) public {
+        require(msg.sender == owner, "Only current owner can transfer ownership");
+        require(_newOwner != address(0), "New owner cannot be zero address");
+        emit OwnershipTransferred(owner, _newOwner);
+        owner = _newOwner;
+    }
+}
